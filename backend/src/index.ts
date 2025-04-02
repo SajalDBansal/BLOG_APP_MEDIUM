59c0fb1537cabf3545cbd215f36d9b60f9191084
SajalDBansal <dutt.sajal2001@gmail.com>
2025-04-02 21:13:36 +0530

    Init Prisma in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,39 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  return c.text('Root directory of Blog app')
 })
+
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Signup endpoint')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Signin endpoint')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Create blog post')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Update blog post')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Get single blog post')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Get multiple blog posts')
+})
+
 
 export default app
